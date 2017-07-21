d9e22c5eab2c1bfb6ce624842c899ef50ccb395f
David Maser <davidemaser@gmail.com>
2017-07-21 11:37:34 -0400

    Add analytics to template object

--- src/functions/FastAnalytics.js
+++ src/functions/FastAnalytics.js
@@ -2,11 +2,7 @@
  * Created by David Maser on 20/07/2017.
  */
 import {Global} from '../config/Global';
-const gtmCode = "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});" +
-  "var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })" +
-  "(window,document,'script','dataLayer','@user.id');</script>";
-const gtAnalyticss = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', '@user.id', 'auto'); ga('send', 'pageview'); </script>";
-const gtmDataLayer = "<script>dataLayer = [{'pageCategory': 'signup','visitorType': 'high-value'}];</script>";
+import {Template} from '../config/Template';
 
 export default class FastAnalytics {
   constructor(option, expression) {
@@ -20,7 +16,7 @@ export default class FastAnalytics {
       case 'gtm':
         let userID = this.expression.indexOf('id:') > -1 ? this.expression.split('id:')[1] : '';
         userID = userID.indexOf(',') > -1 ? userID.split(',')[0] : userID;
-        $(Global.appHead).append(gtmCode.replace('@user.id', userID)).prepend(gtmDataLayer);
+        $(Global.appHead).append(Template.analytics.google.gtmCode.replace('@user.id', userID)).prepend(Template.analytics.google.gtmDataLayer);
         this.register({
           'entries': [
             {'click': 'button'},
@@ -31,7 +27,7 @@ export default class FastAnalytics {
       case 'basic':
         userID = this.expression.indexOf('id:') > -1 ? this.expression.split('id:')[1] : '';
         userID = userID.indexOf(',') > -1 ? userID.split(',')[0] : userID;
-        $(Global.appHead).append(gtAnalyticss.replace('@user.id', userID));
+        $(Global.appHead).append(Template.analytics.google.gtAnalytics.replace('@user.id', userID));
         break;
     }
   }
