bc28a47f16fa52a22fdb7a44f8ae1e35eeb58470
David Maser <davidemaser@gmail.com>
2017-07-31 14:24:55 -0400

    Minor modifications to global namespace

--- src/functions/FastPing.js
+++ src/functions/FastPing.js
@@ -2,12 +2,6 @@
  * Created by David Maser on 31/07/2017.
  */
 export const FastPing = {
-  /**
-   * Creates and loads an image element by url.
-   * @param  {String} url
-   * @return {Promise} promise that resolves to an image element or
-   *                   fails to an Error.
-   */
   requestImage:function(url) {
     return new Promise(function(resolve, reject) {
       let img = new Image();
@@ -16,13 +10,6 @@ export const FastPing = {
       img.src = url + '?random-no-cache=' + Math.floor((1 + Math.random()) * 0x10000).toString(16);
     });
   },
-
-  /**
-   * Pings a url.
-   * @param  {String} url
-   * @param  {Number=} multiplier - optional, factor to adjust the ping by.  0.3 works well for HTTP servers.
-   * @return {Promise} promise that resolves to a ping (ms, float).
-   */
   ping:function(url, multiplier) {
     return new Promise((resolve, reject)=> {
       let start = (new Date()).getTime();
